09c9d04fafbb46c9b09a30d69871bd9defc67159
GaoxiaoyuVV <963477295@qq.com>
2020-03-01 01:35:46 +0800

    add drag and zoom

--- src/assets/common-topo/common-topo.component.ts
+++ src/assets/common-topo/common-topo.component.ts
@@ -14,6 +14,10 @@ export class CommonTopoComponent implements OnInit {
   public link: any;
   public labels: any;
   public node: any;
+  public zoom: any;
+  public transform: any;
+  public g: any;
+  public hull: any;
   constructor() { }
 
   ngOnInit() {
@@ -29,35 +33,109 @@ export class CommonTopoComponent implements OnInit {
       .attr('height', '100%');
     let svgWidth = document.getElementsByTagName('svg')[0].getBoundingClientRect().width;
     let svgHeight = document.getElementsByTagName('svg')[0].getBoundingClientRect().height;
+    this.zoom = this.svg.append('rect')
+      .attr('width', svgWidth)
+      .attr('height', svgHeight)
+      .style('fill', 'none')
+      .style('pointer-events', 'all');
+    this.g = this.svg.append('g')
+      .attr('width', svgWidth)
+      .attr('height', svgHeight);
+    this.link = this.g.append('g')
+      .attr('class', 'links');
+    this.node = this.g.append('g')
+      .attr('class', 'nodesGroup');
+    this.hull = this.g.append('g')
+      .attr('class', 'hulls');
     /* link：连线的连接所用标识(后面为link的长度)
       charge：具体力的装载方式
       center：这个图标的中心点 */
     this.simulation = d3.forceSimulation()
       .force('link', d3.forceLink().id((d: any) => d.index).distance(100))
       .force('charge', d3.forceManyBody())
-      .force('center', d3.forceCenter(svgWidth / 2, svgHeight / 2));
+      .force('center', d3.forceCenter(svgWidth / 2, svgHeight / 2))
+      .on('tick', () => { this.ticked(); });
     this.draw();
+    this.zoomer();
+  }
+
+  public draw() {
+    this.simulation
+      .nodes(this.data.nodes);
+
+    this.simulation.force('link')
+      .links(this.data.links);
+
+    this.link = this.link
+      .selectAll('line')
+      .data(this.data.links)
+      .enter()
+      .append('line')
+      .attr('class', 'path');
+
+    //当D3.js进行到.selectAll('g').data(this.data.nodes).enter()的时候已经将数据分配下去了，所以看起来是先渲染再赋值。
+    this.node = this.node
+      .selectAll('.nodeGroup')
+      .data(this.data.nodes)
+      .enter()
+      .append('g')
+      .attr('class', 'node')
+      .append('circle')
+      .attr('r', 14)
+      .attr('fill', 'red')
+      .attr('class', 'circleClass');
+
+
+    this.labels = this.svg.selectAll('.node')
+      .append("text")
+      .attr('dx', '2em')
+      .attr('dy', 0)
+      .style("fill", "black")
+      .text((d) => {
+        return d.name ? d.name : '';
+      })
+
+    let drag = d3.drag()
+      .on('start', (d) => { this.dragstarted(d) })
+      .on('drag', (d) => { this.dragged(d); })
+      .on('end', (d) => { this.dragended(d); });
+
+    this.svg.selectAll('circle')
+      .call(drag).on("click", () => { console.log("click"); })
+  }
+
+  //add zoom event
+  public zoomer(scale?: any) {
+    //scaleExtent 设置缩放范围
+    let z = d3.zoom()
+      .scaleExtent([0.5, 2])
+      .on('zoom', () => { this.zoomed(); });
+    this.zoom.call(z);
+  }
+
+  public zoomed() {
+    console.log("trigger");
+    this.transform = d3.zoomTransform(this);
+    //获取用来缩放和拖拽的g的坐标位置
+    this.g.attr('transform', d3.event.transform);
   }
 
   //node and link move
   public ticked() {
-    // link use (x1, y1) (x2, y2)
     this.link
       .attr('x1', function (d: any) { return d.source.x; })
       .attr('y1', function (d: any) { return d.source.y; })
       .attr('x2', function (d: any) { return d.target.x; })
       .attr('y2', function (d: any) { return d.target.y; });
-    // node use (cx, cy) and radius
     this.node
       .attr('cx', function (d: any) { return d.x; })
-      .attr('cy', function (d: any) { return d.y; });
-    //change label position
+      .attr('cy', function (d: any) { return d.y; })
     this.labels
       .attr("x", function (d) { return d.x; })
       .attr("y", function (d) { return d.y; });
   }
 
-  //position
+  //drag function
   public dragstarted(d) {
     if (!d3.event.active) { this.simulation.alphaTarget(0.3).restart(); }
     d.fx = d.x;
@@ -73,50 +151,6 @@ export class CommonTopoComponent implements OnInit {
     if (!d3.event.active) { this.simulation.alphaTarget(0); }
     d.fx = null;
     d.fy = null;
-  }
-
-  public draw() {
-    this.link = this.svg.append('g')
-      .attr('class', 'links')
-      .selectAll('line')
-      .data(this.data.links)
-      .enter()
-      .append('line')
-      .attr('class', 'path');
-
-    //当D3.js进行到.selectAll('g').data(this.data.nodes).enter()的时候已经将数据分配下去了，所以看起来是先渲染再赋值。
-    this.node = this.svg.append('g')
-      .attr('class', 'nodesGroup')
-      .selectAll('g')
-      .data(this.data.nodes)
-      .enter()
-      .append('g')
-      .attr('class', 'node')
-      .append('circle')
-      .attr('r', 14)
-      .attr('fill', 'red');
-
-    this.labels = this.svg.selectAll('.node')
-      .append("text")
-      .attr('dx', '2em')
-      .attr('dy', 0)
-      .style("fill", "black")
-      .text(function (d) {
-        return d.name ? d.name : '';
-      })
-
-    this.svg.selectAll('circle').call(d3.drag()
-      .on('start', (d) => { this.dragstarted(d); })
-      .on('drag', (d) => { this.dragged(d); })
-      .on('end', (d) => { this.dragended(d); })
-    );
-
-    this.simulation
-      .nodes(this.data.nodes)
-      .on('tick', () => { this.ticked(); });
-
-    this.simulation.force('link')
-      .links(this.data.links);
   }
 
 }
